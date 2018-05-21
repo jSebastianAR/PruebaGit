368e263d8cca80973416c0640a5f2151f475d115
Jair Sebastian <jSebastian_AR@outlook.com>
2018-05-21 09:08:59 -0500

    Hice un SwitchCase en Rama2

---
+++ SwitchCase.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int x;
+
+	scanf(x);
+
+	switch(x)
+	{
+		case 1:
+			printf("Ingrese a case %d\n", x);
+		break;
+
+		case 2:
+			printf("Ingrese a case %d\n", x);
+		break;
+
+		case 3:
+			printf("Ingrese a case %d\n", x);
+		break;
+	}
+
+}	
